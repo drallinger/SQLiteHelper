23c680a31bd0e638202b08420235037a9a923aaa
Davin Allinger <drallinger2@gmail.com>
2024-04-18 19:18:37 -0400

    Added convertToBoolean method to SQLiteValue class

--- src/main/java/com/drallinger/sqlite/SQLiteValue.java
+++ src/main/java/com/drallinger/sqlite/SQLiteValue.java
@@ -30,6 +30,15 @@ public class SQLiteValue <T> {
         return new SQLiteValue<>(value ? 1 : 0, ValueType.INTEGER);
     }
 
+    public static boolean convertToBoolean(int i){
+        return switch(i){
+            case 1, 0 -> i == 1;
+            default -> throw new IllegalArgumentException(
+                "The given integer must be either 1 or 0 to be converted to a boolean"
+            );
+        };
+    }
+
     public T getValue(){
         return value;
     }
